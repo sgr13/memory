7734a3f1736d115fe5c68e0d701b5774bacf658c
Slawomir Jarzabek <s.g.jarzabek@gmail.com>
2017-09-03 08:55:25 +0200

    CSS & JS - Added 21 cards, added back of the card graphic

--- js/style.js
+++ js/style.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
     $('.card').click(function () {
         $(this).addClass('flipped');
-    })
+        $(this).click(function(){
+            $(this).parent().html('');
+        })
+    });
 });
