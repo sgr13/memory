fca5b66af48d0cc0d5bd8e1f36c8757a596a0ada
Slawomir Jarzabek <s.g.jarzabek@gmail.com>
2017-09-13 23:05:58 +0200

    Game Ready version 1.0

--- js/style.js
+++ js/style.js
@@ -1,7 +1,25 @@
 $(document).ready(function () {
+    var endScore = 0;
+    function changePlayer() {
+        var notActivePlayer = $('.player').not(".activePlayer");
+        $('.activePlayer').removeClass('activePlayer');
+        notActivePlayer.addClass('activePlayer');
+    }
+
+    function checkWinner() {
+        if (endScore == 10) {
+            if ($('#playerOne > .score span').html() > $('#playerTwo > .score span').html()) {
+                alert('Wygrywa pierwszy gracz')
+            } else if ($('#playerOne > .score span').html() < $('#playerTwo > .score span').html()) {
+                alert('Wygrywa drugi gracz');
+            } else {
+                alert('Remis');
+            }
+        }
+    }
+    $('#playerOne').addClass('activePlayer');
 
     var colorArray = ['red', 'yellow', 'blue', 'green', 'white', 'purple', 'pink', 'orange', 'gray', 'brown', 'red', 'yellow', 'blue', 'green', 'white', 'purple', 'pink', 'orange', 'gray', 'brown', 'joker'];
-    var random = randomFrom(colorArray);
 
     $('.container').each(function(){
         var randomPosition = Math.floor(Math.random() * colorArray.length);
@@ -18,10 +36,16 @@ $(document).ready(function () {
             $(this).addClass('flipped');
             if ($('.flipped').last().find('.back').attr('color') == $('.flipped').first().find('.back').attr('color') && $('.flipped').last().find('.back').attr('color')) {
                 setTimeout(function(){
+                    var score = parseInt($('.activePlayer > .score span').html());
+                    $('.activePlayer > .score span').html(score + 1);
                     $('.flipped').css('display', 'none');
                 }, 2000);
+                endScore++;
+                checkWinner();
             } else {
                 setTimeout(function(){
+                    checkWinner();
+                    changePlayer();
                     $('.flipped').removeClass('flipped');
                 }, 1500);
             }
@@ -29,5 +53,4 @@ $(document).ready(function () {
             $(this).addClass('flipped');
         }
     });
-
 });
